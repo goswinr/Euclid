ba12e81a80f268af36e83257c5b597d7744155a2
Goswin Rothenthal <goswin@rothenthal.com>
2026-01-06 14:27:02 +0100

    Adds Line3D struct and improves Line2D
    Adds a new Line3D struct for representing 3D lines.
    
    Improves Line2D by adding VectorX and VectorY members and using them to improve readability and performance.
    Also improves string formatting using string interpolation.

--- Src/Line2D.fs
+++ Src/Line2D.fs
@@ -4,7 +4,7 @@ open System.Runtime.CompilerServices // for [<IsByRefLike; IsReadOnly>]
 open System.Runtime.Serialization // for serialization of struct fields only but not properties via  [<DataMember>] attribute. with Newtonsoft.Json or similar
 
 /// An immutable finite line in 2D. Represented by a 2D start and 2D end point.
-[<Struct;NoEquality;NoComparison>]// because its made up from floats
+[<Struct;NoEquality;NoComparison>] // because it's made up from floats
 [<IsReadOnly>]
 [<DataContract>] // for using DataMember on fields
 type Line2D =
@@ -26,72 +26,91 @@ type Line2D =
     new (fromPt:Pt, toPt:Pt) =
             {FromX=fromPt.X; FromY=fromPt.Y; ToX=toPt.X; ToY=toPt.Y; }
 
-    /// Create a Line2D from 2D start point's x and y  and 2D end point's x and y .
+    /// Create a Line2D from 2D start point's x and y and 2D end point's x and y.
     new (fromX, fromY, toX, toY) =
             {FromX=fromX; FromY=fromY;  ToX=toX; ToY=toY}
+    /// The X component of the line Direction/Vector.
+    member inline ln.VectorX =
+        ln.ToX - ln.FromX
+
+    /// The Y component of the line Direction/Vector.
+    member inline ln.VectorY =
+        ln.ToY - ln.FromY
 
     /// Returns the length of the line.
     member inline ln.Length =
-        let x = ln.ToX-ln.FromX
-        let y = ln.ToY-ln.FromY
+        let x = ln.VectorX
+        let y = ln.VectorY
         sqrt(x*x + y*y)
 
-    /// Returns the square length of the line.
+    /// Returns the squared length of the line.
     member inline ln.LengthSq =
-        let x = ln.ToX-ln.FromX
-        let y = ln.ToY-ln.FromY
+        let x = ln.VectorX
+        let y = ln.VectorY
         x*x + y*y
 
     /// Format 2D line into string including type name, X and Y for start and end points, and Length.
     /// Using nice floating point number formatting.
     override ln.ToString() =
-        sprintf "Euclid.Line2D from X=%s| Y=%s to X=%s| Y=%s Length %s"
-            (Format.float ln.FromX)
-            (Format.float ln.FromY)
-            (Format.float ln.ToX)
-            (Format.float ln.ToY)
-            (Format.float ln.Length)
+        let fx = Format.float ln.FromX
+        let fy = Format.float ln.FromY
+        let tx = Format.float ln.ToX
+        let ty = Format.float ln.ToY
+        let l  = Format.float ln.Length
+        $"Euclid.Line2D from X=%s{fx}|Y=%s{fy} to X=%s{tx}|Y=%s{ty} Length:%s{l}"
 
     /// Format 2D line into string from X and Y for start and end points.
     /// Using nice floating point number formatting.
-    /// But without full type name as in v.ToString()
+    /// But without full type name as in ln.ToString()
     member ln.AsString =
-        sprintf "X=%s| Y=%s to X=%s| Y=%s"
-            (Format.float ln.FromX)
-            (Format.float ln.FromY)
-            (Format.float ln.ToX)
-            (Format.float ln.ToY)
+        let fx = Format.float ln.FromX
+        let fy = Format.float ln.FromY
+        let tx = Format.float ln.ToX
+        let ty = Format.float ln.ToY
+        $"X=%s{fx}|Y=%s{fy} to X=%s{tx}|Y=%s{ty}"
+
+    /// Format 2D line into an F# code string that can be used to recreate the line.
+    member ln.AsFSharpCode =
+        $"Line2D({ln.FromX}, {ln.FromY}, {ln.ToX}, {ln.ToY})"
 
 
-    /// The Start point of the Line2D.
+    /// The start point of the Line2D.
     member inline ln.From =
         Pt(ln.FromX, ln.FromY)
 
-    /// The End point of the Line2D.
+    /// The end point of the Line2D.
     member inline ln.To =
         Pt(ln.ToX, ln.ToY)
 
     /// Same as ln.Vector or ln.Tangent.
     /// The returned vector has the same length as the Line2D.
     member inline ln.Direction =
-        Vc(ln.ToX-ln.FromX, ln.ToY-ln.FromY)
+        Vc(ln.VectorX, ln.VectorY)
 
     /// Same as ln.Tangent or ln.Direction.
     /// The returned vector has the same length as the Line2D.
     member inline ln.Vector =
-        Vc(ln.ToX-ln.FromX, ln.ToY-ln.FromY)
+        Vc(ln.VectorX, ln.VectorY)
+
+
 
     /// Same as ln.Vector or ln.Direction.
     /// The returned vector has the same length as the Line2D.
     member inline ln.Tangent =
-        Vc(ln.ToX-ln.FromX, ln.ToY-ln.FromY)
+        Vc(ln.VectorX, ln.VectorY)
 
     /// Returns a unit-vector of the line Direction.
     member inline ln.UnitTangent =
-        let x = ln.ToX-ln.FromX
-        let y = ln.ToY-ln.FromY
+        let x = ln.VectorX
+        let y = ln.VectorY
         let l = sqrt(x * x  + y * y)
         if UtilEuclid.isTooTiny l then
-            EuclidException.Raisef "Euclid.Line2D.UnitTangent: x:%g and y:%g are too small for creating a unit-vector. Tolerance:%g" x y UtilEuclid.zeroLengthTolerance
+            EuclidErrors.failUnit2 "Line2D.UnitTangent" x y
         UnitVc.createUnchecked (x/l, y/l)
+
+
+
+    // /// Operator to Rotate by Rotation2D around Origin.
+    // /// Multiplies (or applies) a Rotation2D to a 2D line.
+    // static member inline ( *** ) (ln:Line2D, r:Rotation2D) =  ln.Rotate(r)
 
--- Src/Line3D.fs
+++ Src/Line3D.fs
@@ -38,43 +38,60 @@ type Line3D =
     new (fromX, fromY, fromZ, toX, toY, toZ) =
         {FromX=fromX; FromY=fromY; FromZ=fromZ; ToX=toX; ToY=toY; ToZ=toZ}
 
+    /// The X component of the line Direction/Vector.
+    member inline ln.VectorX =
+        ln.ToX - ln.FromX
+
+    /// The Y component of the line Direction/Vector.
+    member inline ln.VectorY =
+        ln.ToY - ln.FromY
+
+    /// The Z component of the line Direction/Vector.
+    member inline ln.VectorZ =
+        ln.ToZ - ln.FromZ
+
     /// Returns the length of the line.
     member inline ln.Length =
-        let x = ln.ToX-ln.FromX
-        let y = ln.ToY-ln.FromY
-        let z = ln.ToZ-ln.FromZ
+        let x = ln.VectorX
+        let y = ln.VectorY
+        let z = ln.VectorZ
         sqrt(x*x + y*y + z*z)
 
-     /// Returns the square length of the line.
+    /// Returns the squared length of the line.
     member inline ln.LengthSq =
-        let x = ln.ToX-ln.FromX
-        let y = ln.ToY-ln.FromY
-        let z = ln.ToZ-ln.FromZ
+        let x = ln.VectorX
+        let y = ln.VectorY
+        let z = ln.VectorZ
         x*x + y*y + z*z
 
     /// Format 3D line into string including type name, X, Y and Z for start and end points, and Length.
-    /// Using nice floating point number formatting .
+    /// Using nice floating point number formatting.
     override ln.ToString() =
-        sprintf "Euclid.Line3D from X=%s| Y=%s| Z=%s to X=%s| Y=%s| Z=%s Length %s"
-            (Format.float ln.FromX)
-            (Format.float ln.FromY)
-            (Format.float ln.FromZ)
-            (Format.float ln.ToX)
-            (Format.float ln.ToY)
-            (Format.float ln.ToZ)
-            (Format.float ln.Length)
+        let fromX = Format.float ln.FromX
+        let fromY = Format.float ln.FromY
+        let fromZ = Format.float ln.FromZ
+        let toX = Format.float ln.ToX
+        let toY = Format.float ln.ToY
+        let toZ = Format.float ln.ToZ
+        let length = Format.float ln.Length
+        $"Euclid.Line3D from X=%s{fromX}|Y=%s{fromY}|Z=%s{fromZ} to X=%s{toX}|Y=%s{toY}|Z=%s{toZ} Length %s{length}"
 
     /// Format 3D line into string from X, Y and Z for start and end points.
     /// Using nice floating point number formatting.
-    /// But without full type name as in v.ToString()
+    /// But without full type name as in ln.ToString()
     member ln.AsString =
-        sprintf "%s, %s, %s to %s, %s, %s"
-            (Format.float ln.FromX)
-            (Format.float ln.FromY)
-            (Format.float ln.FromZ)
-            (Format.float ln.ToX)
-            (Format.float ln.ToY)
-            (Format.float ln.ToZ)
+        let fromX = Format.float ln.FromX
+        let fromY = Format.float ln.FromY
+        let fromZ = Format.float ln.FromZ
+        let toX = Format.float ln.ToX
+        let toY = Format.float ln.ToY
+        let toZ = Format.float ln.ToZ
+        $"%s{fromX}, %s{fromY}, %s{fromZ} to %s{toX}, %s{toY}, %s{toZ}"
+
+    /// Format 3D line into an F# code string that can be used to recreate the line.
+    member ln.AsFSharpCode =
+        $"Line3D({ln.FromX}, {ln.FromY}, {ln.FromZ}, {ln.ToX}, {ln.ToY}, {ln.ToZ})"
+
 
     /// The start point of the Line3D.
     member inline ln.From =
@@ -84,89 +101,35 @@ type Line3D =
     member inline ln.To =
         Pnt(ln.ToX, ln.ToY, ln.ToZ)
 
+
     /// Same as ln.Vector or ln.Tangent.
     /// The returned vector has the same length as the Line3D.
     member inline ln.Direction =
-        Vec(ln.ToX-ln.FromX, ln.ToY-ln.FromY, ln.ToZ-ln.FromZ)
+        Vec(ln.VectorX, ln.VectorY, ln.VectorZ)
 
     /// Same as ln.Tangent or ln.Direction.
     /// The returned vector has the same length as the Line3D.
     member inline ln.Vector =
-        Vec(ln.ToX-ln.FromX, ln.ToY-ln.FromY, ln.ToZ-ln.FromZ)
+        Vec(ln.VectorX, ln.VectorY, ln.VectorZ)
+
 
     /// Same as ln.Vector or ln.Direction.
     /// The returned vector has the same length as the Line3D.
     member inline ln.Tangent =
-        Vec(ln.ToX-ln.FromX, ln.ToY-ln.FromY, ln.ToZ-ln.FromZ)
+        Vec(ln.VectorX, ln.VectorY, ln.VectorZ)
 
     /// Returns a unit-vector of the line Direction.
     member inline ln.UnitTangent =
-        let x = ln.ToX-ln.FromX
-        let y = ln.ToY-ln.FromY
-        let z = ln.ToZ-ln.FromZ
+        let x = ln.VectorX
+        let y = ln.VectorY
+        let z = ln.VectorZ
         let l = sqrt(x * x  + y * y + z * z)
         if UtilEuclid.isTooTiny l then
-            EuclidException.Raisef "Euclid.Line3D.UnitTangent: x:%g, y:%g and z:%g are too small for creating a unit-vector. Tolerance:%g" x y z UtilEuclid.zeroLengthTolerance
+            EuclidErrors.failUnit3 "Line3D.UnitTangent" x y z
         let s = 1.0 / l
         UnitVec.createUnchecked (x*s, y*s, z*s)
 
-    /// Multiplies (or applies) a Quaternion to a 3D line .
-    /// The resulting line has the same length as the input.
-    member inline l.Rotate (q:Quaternion) =
-        // adapted from https://github.com/mrdoob/three.js/blob/dev/src/math/Vector3.js
-        let u = l.FromX
-        let v = l.FromY
-        let w = l.FromZ
-        let x = l.ToX
-        let y = l.ToY
-        let z = l.ToZ
-        let qx = q.X
-        let qy = q.Y
-        let qz = q.Z
-        let qw = q.W
-        let tu = 2.0 * ( qy * w - qz * v)
-        let tv = 2.0 * ( qz * u - qx * w)
-        let tw = 2.0 * ( qx * v - qy * u)
-        let tx = 2.0 * ( qy * z - qz * y)
-        let ty = 2.0 * ( qz * x - qx * z)
-        let tz = 2.0 * ( qx * y - qy * x)
-        Line3D( u + qw * tu + qy * tw - qz * tv ,
-                v + qw * tv + qz * tu - qx * tw ,
-                w + qw * tw + qx * tv - qy * tu ,
-                x + qw * tx + qy * tz - qz * ty ,
-                y + qw * ty + qz * tx - qx * tz ,
-                z + qw * tz + qx * ty - qy * tx )
 
-    /// Multiplies (or applies) a Quaternion to a 3D line around a given center point.
-    /// The resulting line has the same length as the input.
-    member inline l.RotateWithCenter (cen:Pnt, q:Quaternion) =
-        let cX = cen.X
-        let cY = cen.Y
-        let cZ = cen.Z
-        let u = l.FromX - cX
-        let v = l.FromY - cY
-        let w = l.FromZ - cZ
-        let x = l.ToX   - cX
-        let y = l.ToY   - cY
-        let z = l.ToZ   - cZ
-        let qx = q.X
-        let qy = q.Y
-        let qz = q.Z
-        let qw = q.W
-        let tu = 2.0 * ( qy * w - qz * v)
-        let tv = 2.0 * ( qz * u - qx * w)
-        let tw = 2.0 * ( qx * v - qy * u)
-        let tx = 2.0 * ( qy * z - qz * y)
-        let ty = 2.0 * ( qz * x - qx * z)
-        let tz = 2.0 * ( qx * y - qy * x)
-        Line3D( u + qw * tu + qy * tw - qz * tv + cX ,
-                v + qw * tv + qz * tu - qx * tw + cY,
-                w + qw * tw + qx * tv - qy * tu + cZ ,
-                x + qw * tx + qy * tz - qz * ty + cX ,
-                y + qw * ty + qz * tx - qx * tz + cY ,
-                z + qw * tz + qx * ty - qy * tx + cZ )
-
-
-    /// Rotate by Quaternion around Origin.
-    /// Multiplies (or applies) a Quaternion to a 3D line.
-    static member inline ( *** ) (ln:Line3D, q:Quaternion) =  ln.Rotate(q)
+    // /// Operator to Rotate by Quaternion around Origin.
+    // /// Multiplies (or applies) a Quaternion to a 3D line.
+    // static member inline ( *** ) (ln:Line3D, q:Quaternion) =  ln.Rotate(q)
